9aa703ead4155ffa294eb66c9d04d82dfc824580
uznimzu <104189608+uznimzu@users.noreply.github.com>
2024-03-07 22:09:07 +0900

    [level 2] Title: 숫자의 표현, Time: 0.38 ms, Memory: 52.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int start = 1;
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum += i;
+            while (sum > n) {
+                sum -= start;
+                start++;
+            }
+            if (sum == n) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
