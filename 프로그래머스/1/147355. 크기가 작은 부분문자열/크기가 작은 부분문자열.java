8341c1d68bba0647c5ce4a03bfae15a71ef6c09c
uznimzu <104189608+uznimzu@users.noreply.github.com>
2024-03-13 21:48:22 +0900

    Update 크기가 작은 부분문자열.java

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -1,3 +1,11 @@
+/*[풀이방법]
+1. p의 길이는 1이상 18 이하 -> Long 사용
+2. p의 길이와 문자열을 정수로 변환
+3. 비교하는 반복문 실행
+    3-1. for문의 길이는 t의 길이에서 p를 뺀 후 1을 더한 값이 됨(마치 날짜와 같은 개념)
+    3-2. 문자열 t를 subString을 이용하여 문자열 잘라내기
+    3-3. 잘라낸 문자열과 p를 서로 비교한 후, p가 크면 1씩 증가*/
+
 class Solution {
     public int solution(String t, String p) {
         int answer = 0;
