2c64bb2f27cf73f1b6933e62d44dc1757ccac791
uznimzu <104189608+uznimzu@users.noreply.github.com>
2024-04-04 21:32:20 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.09 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
@@ -3,10 +3,15 @@ class Solution {
         long answer = 0;
         long result = 0; 
         
+        // count의 범위까지 i를 순회하면서 price와 곱하기
+        // 곱한 값을 저장해두기
         for(int i = 1; i<=count; i++){
             long pay = price * i;
             result += pay;
         }
+        
+        // money와 result를 뺐을 때, 0보다 크면 answer 반환
+        // 그외는 0을 반환
         answer = result - money;
         return answer>0 ? answer: 0;
     }
