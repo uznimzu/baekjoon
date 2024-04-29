aa2411562afd1dd89a11ee1ac5949507498fff6d
uznimzu <104189608+uznimzu@users.noreply.github.com>
2024-04-29 15:24:30 +0900

    [level 1] Title: K번째수, Time: 0.03 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,23 +1,54 @@
-import java.util.Arrays;
-
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        int[] result = {};
-
-        // command에 들어있는 각 원소들을 분리하고, 새로운 변수에 할당
+        
         for(int i=0; i<commands.length; i++){
-                int a = commands[i][0]-1;
-                int b = commands[i][1];
-                int c = commands[i][2]-1; 
-    
-                // 원래 배열을 복사하고, a와 b사이에 있는 것들만 갖고온 후 정렬
-                result = Arrays.copyOfRange(array, a, b);
-                Arrays.sort(result);
+            int a = commands[i][0]-1;
+            int b = commands[i][1];
+            int c = commands[i][2]-1;
+
+            // 원본 배열의 일부를 복사합니다.
+            int[] tempArray = new int[b - a];
+            for (int j = 0; j < tempArray.length; j++) {
+                tempArray[j] = array[a + j];
+            }
+
+            // 퀵 정렬을 실행합니다.
+            quickSort(tempArray, 0, tempArray.length - 1);
             
-                // result[c]에 있는 게 정답
-                answer[i] = result[c]; 
+            // 정렬된 배열에서 k번째 수를 찾습니다.
+            answer[i] = tempArray[c];
         }
         return answer;
+    }
+    
+    // 퀵 정렬 메서드
+    private void quickSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int pivotIndex = partition(arr, left, right);
+            quickSort(arr, left, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, right);
+        }
+    }
+    
+    // 분할 메서드
+    private int partition(int[] arr, int left, int right) {
+        int pivot = arr[right];
+        int i = (left - 1);
+        for (int j = left; j < right; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                // arr[i]와 arr[j]를 교환
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        // arr[i+1]와 pivot(=arr[right])을 교환
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[right];
+        arr[right] = temp;
+        
+        return i + 1;
     }
 }
